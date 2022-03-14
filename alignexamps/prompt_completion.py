5d55561767475f9cd10a5dfccdb84fd4c4cf3d0d
Ali Zaidi <alikaz.zaidi@gmail.com>
2022-03-14 15:21:29 +0000

    INITIAL COMMIT: of prompt completer; todo add a tree struct for saving generations

--- alignexamps/prompt_completion.py
+++ alignexamps/prompt_completion.py
@@ -2,6 +2,8 @@
 Prompt completion API for OpenAI decoder models
 """
 
+from typing import Dict
+
 import os
 import openai
 import torch
@@ -10,23 +12,17 @@ import numpy as np
 import torch
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util as st_utils
-import json
+from dotenv import load_dotenv
 
-GPU = 0
-if torch.cuda.is_available():
-    print("CUDA available!")
-    torch.cuda.set_device(GPU)
-else:
-    print("CUDA unavailable :(")
-
-# source = 'huggingface'  # select from ['openai', 'huggingface']
-source = "openai"
-# planning_lm_id = 'gpt2-large'  # see comments above for all options
-planning_lm_id = "davinci-msft"
-translation_lm_id = "stsb-roberta-large"  # see comments above for all options
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+load_dotenv()
 
 # default parameters
+source = "openai"
+# planning_lm_id = 'gpt2-large'  # see comments above for all options
+# available_engines = openai.Engine.list()
+planning_lm_id = "davinci-msft"
+
+
 MAX_STEPS = 20  # maximum number of steps to be generated
 CUTOFF_THRESHOLD = (
     0.8  # early stopping threshold based on matching score and likelihood score
@@ -34,11 +30,11 @@ CUTOFF_THRESHOLD = (
 P = 0.5  # hyperparameter for early stopping heuristic to detect whether Planning LM believes the plan is finished
 BETA = 0.3  # weighting coefficient used to rank generated samples
 if source == "openai":
-    openai.api_key = OPENAI_KEY
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     sampling_params = {
-        "max_tokens": 10,
-        "temperature": 0.6,
-        "top_p": 0.9,
+        "max_tokens": 64,
+        "temperature": 0.7,
+        "top_p": 1.0,
         "n": 10,
         "logprobs": 1,
         "presence_penalty": 0.5,
@@ -47,7 +43,7 @@ if source == "openai":
     }
 elif source == "huggingface":
     sampling_params = {
-        "max_tokens": 10,
+        "max_tokens": 256,
         "temperature": 0.1,
         "top_p": 0.9,
         "num_return_sequences": 10,
@@ -57,81 +53,129 @@ elif source == "huggingface":
         "return_dict_in_generate": True,
         "do_sample": True,
     }
+else:
+    raise ValueError("source must be either 'openai' or 'huggingface'")
 
 
-class PromptCompletion:
-    def __init__(self, engine_id: str = "davinci", max_tokens: int = 256):
+def lm_engine(source: str, planning_lm_id: str, device):
+    if source == "huggingface":
+        from transformers import AutoModelForCausalLM, AutoTokenizer
 
-        self.engine_id = engine_id
-        self.max_tokens = max_tokens
+        tokenizer = AutoTokenizer.from_pretrained(planning_lm_id)
+        model = AutoModelForCausalLM.from_pretrained(
+            planning_lm_id, pad_token_id=tokenizer.eos_token_id
+        ).to(device)
 
-    def complete_prompt(self, prompt: str = "", **kwargs):
-        """[summary]
+    def _generate(prompt, sampling_params: Dict = sampling_params):
+        if source == "openai":
+            response = openai.Completion.create(
+                engine=planning_lm_id, prompt=prompt, **sampling_params
+            )
+            generated_samples = [
+                response["choices"][i]["text"] for i in range(sampling_params["n"])
+            ]
+            # calculate mean log prob across tokens
+            mean_log_probs = [
+                np.mean(response["choices"][i]["logprobs"]["token_logprobs"])
+                for i in range(sampling_params["n"])
+            ]
+        elif source == "huggingface":
+            input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+            prompt_len = input_ids.shape[-1]
+            output_dict = model.generate(
+                input_ids,
+                max_length=prompt_len + sampling_params["max_tokens"],
+                **sampling_params,
+            )
+            # discard the prompt (only take the generated text)
+            generated_samples = tokenizer.batch_decode(
+                output_dict.sequences[:, prompt_len:]
+            )
+            # calculate per-token logprob
+            vocab_log_probs = torch.stack(output_dict.scores, dim=1).log_softmax(
+                -1
+            )  # [n, length, vocab_size]
+            token_log_probs = (
+                torch.gather(
+                    vocab_log_probs, 2, output_dict.sequences[:, prompt_len:, None]
+                )
+                .squeeze(-1)
+                .tolist()
+            )  # [n, length]
+            # truncate each sample if it contains '\n' (the current step is finished)
+            # e.g. 'open fridge\n<|endoftext|>' -> 'open fridge'
+            for i, sample in enumerate(generated_samples):
+                stop_idx = sample.index("\n") if "\n" in sample else None
+                generated_samples[i] = sample[:stop_idx]
+                token_log_probs[i] = token_log_probs[i][:stop_idx]
+            # calculate mean log prob across tokens
+            mean_log_probs = [
+                np.mean(token_log_probs[i])
+                for i in range(sampling_params["num_return_sequences"])
+            ]
+        generated_samples = [sample.strip().lower() for sample in generated_samples]
+        return generated_samples, mean_log_probs
 
-        Parameters
-        ----------
-        prompt : str, optional
-            [description], by default ""
-        Allowable kwargs
-            {
-                "prompt": "Say this is a test",
-                "max_tokens": 5,
-                "temperature": 1,
-                "top_p": 1,
-                "n": 1,
-                "stream": false,
-                "logprobs": null,
-                "stop": "\n"
-            }
+    return _generate
 
 
-        Returns
-        -------
-        [type]
-            [description]
-        """
+if __name__ == "__main__":
 
-        return openai.Completion.create(
-            engine=self.engine_id, prompt=prompt, max_tokens=self.max_tokens, **kwargs
-        )
+    GPU = 0
+    if torch.cuda.is_available():
+        print("CUDA available!")
+        torch.cuda.set_device(GPU)
+    else:
+        print("CUDA unavailable :(")
 
+    # source = 'huggingface'  # select from ['openai', 'huggingface']
+    source = "openai"
+    # planning_lm_id = 'gpt2-large'  # see comments above for all options
+    planning_lm_id = "davinci-msft"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    default_prompt = """Hello, my name is GPT and"""
+    import argparse
 
-start_sequence = "\nNPC:"
-restart_sequence = "\nAdventurer: "
-
-# get back story, and add it to the prompt
-with open("backstory.txt", "r") as input_file:
-    backstory = input_file.read()
-prompt = (
-    backstory
-    + "\nThe following is a conversation between an Adventurer and an NPC in the Divine City.\n"
-)
-
-# initial greeting
-greeting = start_sequence + " greetings adventurer! how can i help you?"
-print(greeting)
-prompt += greeting
-
-while True:
-    # get user message
-    message = input("Adventurer: ")
-    prompt += restart_sequence + message + start_sequence
-
-    # get response from api
-    response = openai.Completion.create(
-        engine="text-davinci-001",
-        prompt=prompt,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " NPC:", " Adventurer:"],
+    parser = argparse.ArgumentParser(description="Language Model completion arena")
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default=default_prompt,
+        help="Prompt to initialize language model",
     )
-    npc_message = response["choices"][0]["text"]
-    print(f"NPC:{npc_message}")
+    parser.add_argument(
+        "--engine-id",
+        type=str,
+        default=planning_lm_id,
+        help="Engine ID, i.e., davinici, babbage",
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        default="openai",
+        help="Source of langauge model, i.e., openai, huggingface",
+    )
 
-    # add response to prompt
-    prompt += npc_message + restart_sequence
+    args, _ = parser.parse_known_args()
+    generator = lm_engine(args.source, args.engine_id, device)
+    response = generator(args.prompt, sampling_params)
+    print(response)
+
+    def generate_tree(
+        tree_len: int = 10,
+        prompt: str = default_prompt,
+        lm_model=generator,
+        params: Dict = sampling_params,
+    ):
+
+        iteration = 0
+        while iteration < tree_len:
+            prompt += lm_model(prompt, params)[0][0]
+            iteration += 1
+
+        return prompt
+
+    # TODO: create a tree structure to capture prompt trees
+    generation = generate_tree()
+    print(generation)
